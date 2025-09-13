feb7cb0a30f37caf43a722d025a1ffa4988c5744
anurag bagri <anurag123bagri@gmail.com>
2025-09-13 09:31:38 +0530

    added test

--- account_data/tests/account_data.ts
+++ account_data/tests/account_data.ts
@@ -2,15 +2,43 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AccountData } from "../target/types/account_data";
 
-describe("account_data", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+declare('account data' , () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  
+  const payer = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.AccountData as anchor.Program<AccountData>;
 
-  const program = anchor.workspace.accountData as Program<AccountData>;
+  //generate the keypair for the addressInfo account 
+  const addressInfoAccount = new Keypair();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+  it('Create the address info account ', async() => {
+    console.log(`payer address : ${payer.publickey}`);
+    console.log(`address_info account address : ${addressInfoAccount.publickey}`);
+
+    // instruction INx data 
+    const addressInfo = {
+      name : 'ANURAG BAGRI',
+      house_number : 136,
+      street : 'near rims hospital',
+      city : 'utopia'
+    };
+    
+        await program.methods
+      .createAddressInfo(addressInfo.name, addressInfo.house_number, addressInfo.street, addressInfo.city)
+      .accounts({
+        addressInfo: addressInfoAccount.publicKey,
+        payer: payer.publicKey,
+      })
+      .signers([addressInfoAccount])
+      .rpc();
+  });
+   it("Read the new account's data", async () => {
+    const addressInfo = await program.account.addressInfo.fetch(addressInfoAccount.publicKey);
+    console.log(`Name     : ${addressInfo.name}`);
+    console.log(`House Num: ${addressInfo.house_number}`);
+    console.log(`Street   : ${addressInfo.street}`);
+    console.log(`City     : ${addressInfo.city}`);
   });
 });
