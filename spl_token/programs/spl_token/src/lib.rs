45e37b4ceb2d3db13dfd6a967129fb63531b40d0
anurag bagri <anurag123bagri@gmail.com>
2025-09-26 23:53:57 +0530

    added anchor_spl and metadata_token dep

--- spl_token/programs/spl_token/src/lib.rs
+++ spl_token/programs/spl_token/src/lib.rs
@@ -1,1 +1,32 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint , MintTo , Token};
 
+
+
+declare_id!("")
+#[program]
+pub mod create_mint_anchor {
+    use super::*;
+
+    pub fn create_mint_metadata_mint_to() -> Result<()> {
+ 
+    }
+
+
+    #[derive(Accounts)]
+    pub struct CreateMintAnchor<'info> {
+        #[account(mut)]
+        pub signer : Signer<'info>,
+
+        // mint account initialized on-chain 
+        #[account(
+            init,
+            payer= signer,
+            space = 82, // spl token mint size 
+            seeds = [b"mint"],
+            bump,
+        )]
+
+        pub mint : Account<'info , Mint>
+    }
+}
