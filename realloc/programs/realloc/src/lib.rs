0f900b2455e75f98511e0b59500c1968e522f62a
anurag bagri <anurag123bagri@gmail.com>
2025-09-27 13:54:57 +0530

    Added account realloc program

--- realloc/programs/realloc/src/lib.rs
+++ realloc/programs/realloc/src/lib.rs
@@ -4,43 +4,55 @@ declare_id!("85kxahUDPgBTPHjChigZdcoCEPrca6qwGVQaHLkNUHMs");
 
 #[program]
 pub mod anchor_realloc {
+
+    use anchor_lang::solana_program::message::Message;
+
     use super::*;
 
-    pub fn initialize() -> Result<()> {
-       
+    pub fn initialize(ctx: Context<InitializeAccount>, input: String) -> Result<()> {
+        ctx.accounts.message_account.msg = input;
+        Ok(())
     }
 
-    pub fn update() -> Result<()> {
-
-    }
-
+    pub fn update(ctx: Context<UpdateAccount>, input: String) -> Result<()> {}
 
     #[derive(Accounts)]
     #[instruction(input : String)]
     pub struct InitializeAccount<'info> {
         #[account(mut)]
-        pub signer : Signer<'info>,
+        pub signer: Signer<'info>,
         #[account(
             init,
             payer=signer,
-            space = 
+            space = Message::required_space(input)
         )]
-
+        pub message_account: Account<'info, Message>,
+        pub system_program: Program<'info, System>,
     }
 
     #[derive(Accounts)]
     #[instruction(input: String)]
     pub struct UpdateAccount<'info> {
+        #[account(mut)]
+        pub signer: Signer<'info>,
 
+        #[account(
+            mut,
+            realloc = Message::required_space(input.len()),
+            realloc::payer = signer,
+            realloc::zero = true
+        )]
+        pub message_account: Account<'info, Message>,
+        pub system_program: Program<'info, System>,
     }
 
     #[account]
     pub struct Message {
-        pub msg : String
+        pub msg: String,
     }
 
     impl Message {
-        pub fn required_space(input_length : usize) -> usize {
+        pub fn required_space(input_length: usize) -> usize {
             8 + 4 + input_length
         }
     }
