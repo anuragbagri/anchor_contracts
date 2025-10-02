1149ea94a021d84d0e0ddc4739479d0a09118d8d
anurag bagri <anurag123bagri@gmail.com>
2025-10-02 17:19:06 +0530

    cretaed mint account

--- nft_minter/programs/nft_minter/src/lib.rs
+++ nft_minter/programs/nft_minter/src/lib.rs
@@ -1,16 +1,40 @@
 use anchor_lang::prelude::*;
-
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create
+    }
+}
 declare_id!("QdTg9Ufh35JZBjVTaKdsz75KQi87CiX5JXq21LnrfBp");
+
 
 #[program]
 pub mod nft_minter {
-    use super::*;
+    use anchor_spl::token::Mint;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    use super::*;
+    pub fn mint_nft() -> Result<()> {
+
+    }
+
+    #{derive(Accounts)}
+    pub struct CreateNft<'info> {
+        #[account(mut)]
+        pub payer : Signer<'info>,
+        // derive mint account 
+        #[account(
+            init,
+            payer=payer,
+            mint::decimals = 0,
+            mint::authority = payer.key(),
+            mint::freeze_authority = payer.key()
+        )]
+        pub mint_account : Account<'info,Mint>
+
+        // create metadata account 
+        #[account(
+            mut,
+            seeds = [b"metadata", token_meta]
+        )]
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
